a560823d01e68a5fa74668f230e785cffb016e7c
Akemy Mory <110566982+akemymory@users.noreply.github.com>
2023-09-27 21:02:20 +0100

    Java learning

---
+++ conversor-fahrenheit/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int temperaturaCelcius = 15;
+        int temperaturaFahrenheit = (int) (temperaturaCelcius * 1.8) + 32;
+        System.out.println(temperaturaFahrenheit);
+    }
+}
