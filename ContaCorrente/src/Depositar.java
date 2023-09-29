a3e939aab1ede6060fef512ea2fbbba8ff785f7c
Akemy Mory <110566982+akemymory@users.noreply.github.com>
2023-09-29 21:36:57 +0100

    Java practice

---
+++ ContaCorrente/src/Depositar.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+    public class Depositar {
+        public static void main(String[] args) {
+            Scanner valor = new Scanner(System.in);
+            System.out.println("Informe o valor que deseja depositar:");
+            double valorDepositado = valor.nextDouble();
+
+
+        }
+    }
+
+
