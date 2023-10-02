24cbdeecf2372eecd57d3f04f22f706f96d58c7b
Akemy Mory <110566982+akemymory@users.noreply.github.com>
2023-10-02 09:58:52 +0100

    Java Object-Oriented Programming practice

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -14,6 +15,13 @@ public class Principal {
        meuFilme.avalia(5);
        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
        System.out.println(meuFilme.pegaMedia());
+
+       Serie lost = new Serie();
+       lost.setNome("Lost");
+       lost.setAnoDeLancamento(2000);
+       lost.exibeFichaTecnica();
+       lost.setTemporadas(6);
+       lost.setEpisodiosPorTemporada(20);
 
     }
 }
