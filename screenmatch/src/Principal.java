94709b67c9eb965d224795ef96d6231d394d691a
Akemy Mory <110566982+akemymory@users.noreply.github.com>
2023-10-12 10:00:35 +0100

    Constructors Java Study

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -8,12 +8,9 @@ import java.util.ArrayList;
 
 public class Principal {
     public static void main(String[] args) {
-       Filme meuFilme =  new Filme();
-       meuFilme.setNome("O poderoso chefão");
-       meuFilme.setAnoDeLancamento(1970);
+       Filme meuFilme =  new Filme("O poderoso chefão", 1970);
        meuFilme.setDuracaoEmMinutos(180);
        System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
-
 
        meuFilme.exibeFichaTecnica();
        meuFilme.avalia(8);
@@ -22,18 +19,14 @@ public class Principal {
        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
        System.out.println(meuFilme.pegaMedia());
 
-       Serie lost = new Serie();
-       lost.setNome("Lost");
-       lost.setAnoDeLancamento(2000);
+       Serie lost = new Serie("Lost", 2000);
        lost.exibeFichaTecnica();
        lost.setTemporadas(6);
        lost.setEpisodiosPorTemporada(20);
        lost.setMinutosPorEpisodio(50);
        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
 
-       Filme outroFilme =  new Filme();
-       outroFilme.setNome("Avatar");
-       outroFilme.setAnoDeLancamento(2023);
+       Filme outroFilme =  new Filme("Avatar", 2023);
        outroFilme.setDuracaoEmMinutos(200);
 
        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
@@ -51,10 +44,8 @@ public class Principal {
        episodio.setTotalVisualizacoes(300);
        filtro.filtra(episodio);
 
-       var filmeDoPaulo = new Filme();
+       var filmeDoPaulo = new Filme("Dogville", 2003);
        filmeDoPaulo.setDuracaoEmMinutos(200);
-       filmeDoPaulo.setNome("Dogville");
-       filmeDoPaulo.setAnoDeLancamento(2003);
        filmeDoPaulo.avalia(10);
 
        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
